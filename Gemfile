635465bfd33d7753acb68772a7b3b6e98f5d6af7
Anjali <anjali@anjali.(none)>
2013-06-09 08:23:06 +0530

    first

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'therubyracer'
+
+group :production do
+gem 'mysql2'
+gem 'passenger'
+gem 'rack'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '~> 2.0.0'
+gem 'execjs'
+gem 'spree', '1.1.3'
+
+group :development do
+   gem 'capistrano'
+   gem 'rvm-capistrano'
+end
+
+gem 'spree_usa_epay'
+gem 'spree_skrill'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testing::Application.config.secret_token = 'db5d89306271131e9443f4d6aa3a40e28ce3413fb43beb4dc4ebfe1c06991955876e4821969cde6d8c2af3ab77fcf9a6b86adedb9d8ff5be6ae90a1ff0ac1371'
