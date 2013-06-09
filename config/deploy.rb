e72efc9e68d6e3e63c1cd585e5a94615e64c9b59
Anjali <anjali@anjali.(none)>
2013-06-09 08:33:17 +0530

    2nd

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+set :rvm_ruby_string, '1.9.3'
+
+server "175.41.130.195", :web, :app, :db, :primary => true
+
+set :application, "testing"
+set :user, "facoteam"
+set :deploy_to, "/home/facoteam/stores/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :rails_env, "production"
+
+set :repository, "git@github.com:Anjali-Veer/testing.git"
+set :scm, :git
+set :branch, "master"
+
+
+set :shell, '/bin/bash'
+
+set :rvm_type, :system
+
+default_run_options[:pty]=true
+default_run_options[:shell]='/bin/bash --login'
+ssh_options[:forward_agent]=true
+
+after 'deploy', 'deploy:cleanup'
+
+namespace :deploy do
+        desc "Override deploy:cold to NOT run migrations - there's no database"
+        task :cold do
+                update
+                start
+        end
+end
